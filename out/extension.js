eb3315cc0148f6aa07a437987838fea78fb3d032
lujiejie <lujiejie@bixin.cn>
2021-06-30 02:39:54 +0800

    Add lsp support

--- out/extension.js
+++ out/extension.js
@@ -1,92 +1,168 @@
 "use strict";
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var vscode = require("vscode");
-var cp = require("child_process");
-var path = require("path");
-var os = require("os");
-var fs = require("fs");
-var FunnyDocumentFormatter = /** @class */ (function () {
-    function FunnyDocumentFormatter() {
-    }
-    FunnyDocumentFormatter.prototype.provideDocumentFormattingEdits = function (document, options, token) {
-        console.log('formatting');
-        return new Promise(function (resolve, reject) {
-            var t0 = Date.now();
-            // let env = getToolsEnvVars();
-            var stdout = '';
-            var stderr = '';
-            // Use spawn instead of exec to avoid maxBufferExceeded error
-            var funnyPath = process.env['FUNNY_BIN'];
-            if (!funnyPath) {
-                var goPath = process.env['GOPATH'];
-                if (!goPath) {
-                    goPath = path.join(os.homedir(), 'go');
-                }
-                funnyPath = path.join(goPath, 'bin', 'funny');
-                if (!fs.existsSync(funnyPath)) {
-                    console.error("funny path " + funnyPath + " not exists");
-                }
-            }
-            console.log(funnyPath);
-            var p = cp.spawn(funnyPath, ['--format']);
-            p.stdout.setEncoding('utf8');
-            p.stdout.on('data', function (data) {
-                console.log(data);
-                stdout += data;
-            });
-            p.stderr.on('data', function (data) {
-                stderr += data;
-            });
-            p.on('error', function (err) {
-                if (err && err.code === 'ENOENT') {
-                    // promptForMissingTool(formatTool);
-                    console.log(err);
-                    return reject();
-                }
-            });
-            p.on('close', function (code) {
-                if (code !== 0) {
-                    console.log(stderr);
-                    return reject(stderr);
-                }
-                // Return the complete file content in the edit.
-                // VS Code will calculate minimal edits to be applied
-                var fileStart = new vscode.Position(0, 0);
-                var fileEnd = document.lineAt(document.lineCount - 1).range.end;
-                var textEdits = [new vscode.TextEdit(new vscode.Range(fileStart, fileEnd), stdout)];
-                var timeTaken = Date.now() - t0;
-                /* __GDPR__
-                     "format" : {
-                        "tool" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
-                        "timeTaken": { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth", "isMeasurement": true }
-                     }
-                 */
-                // sendTelemetryEvent('format', { tool: formatTool }, { timeTaken });
-                if (timeTaken > 750) {
-                    console.log("Formatting took too long(" + timeTaken + "ms). Format On Save feature could be aborted.");
-                }
-                return resolve(textEdits);
-            });
-            p.stdin.end(document.getText());
-        });
-    };
-    return FunnyDocumentFormatter;
-}());
-var FunnyOnTypingFormatter = /** @class */ (function () {
-    function FunnyOnTypingFormatter() {
-    }
-    FunnyOnTypingFormatter.prototype.provideOnTypeFormattingEdits = function (document, position, ch, options, token) {
-        throw new Error("Method not implemented.");
-    };
-    return FunnyOnTypingFormatter;
-}());
+exports.deactivate = exports.activate = exports.languageClient = void 0;
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const vscode_1 = require("vscode");
+const node_1 = require("vscode-languageclient/node");
+let crashCount = 0;
+// errorKind refers to the different possible kinds of gopls errors.
+var errorKind;
+(function (errorKind) {
+    errorKind[errorKind["initializationFailure"] = 0] = "initializationFailure";
+    errorKind[errorKind["crash"] = 1] = "crash";
+    errorKind[errorKind["manualRestart"] = 2] = "manualRestart";
+})(errorKind || (errorKind = {}));
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 function activate(context) {
-    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('funny', new FunnyDocumentFormatter()));
-    // context.subscriptions.push(
-    // 	vscode.languages.registerOnTypeFormattingEditProvider('funny', new FunnyOnTypingFormatter(), '\n'));
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "vscode-funny" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('vscode-funny.helloWorld', () => {
+        // The code you place here will be executed every time your command is executed
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Hello World from vscode-funny!');
+    });
+    console.log('Congratulations, your extension "vscode-funny" is now active!');
+    // Options to control the language client
+    let clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'funny' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: vscode_1.workspace.createFileSystemWatcher('**/.clientrc')
+        },
+        initializationFailedHandler: (err) => {
+            console.log(err);
+            return true;
+        },
+        errorHandler: {
+            error: (error, message, count) => {
+                console.log('Congratulations, your extension "vscode-funny" is now errro!');
+                // Allow 5 crashes before shutdown.
+                if (count < 5) {
+                    return node_1.ErrorAction.Continue;
+                }
+                vscode.window.showErrorMessage(`Error communicating with the language server: ${error}: ${message}.`);
+                return node_1.ErrorAction.Shutdown;
+            },
+            closed: () => {
+                // Allow 5 crashes before shutdown.
+                crashCount++;
+                if (crashCount < 5) {
+                    return node_1.CloseAction.Restart;
+                }
+                console.log('The connection to funnypls has been closed. The funnypls server may have crashed.', errorKind.crash);
+                return node_1.CloseAction.DoNotRestart;
+            }
+        },
+        middleware: {
+            provideCompletionItem: (document, position, context, token, next) => __awaiter(this, void 0, void 0, function* () {
+                const list = yield next(document, position, context, token);
+                if (!list) {
+                    return list;
+                }
+                const items = Array.isArray(list) ? list : list.items;
+                // Give all the candidates the same filterText to trick VSCode
+                // into not reordering our candidates. All the candidates will
+                // appear to be equally good matches, so VSCode's fuzzy
+                // matching/ranking just maintains the natural "sortText"
+                // ordering. We can only do this in tandem with
+                // "incompleteResults" since otherwise client side filtering is
+                // important.
+                if (!Array.isArray(list) && list.isIncomplete && list.items.length > 1) {
+                    let hardcodedFilterText = items[0].filterText;
+                    if (!hardcodedFilterText) {
+                        // tslint:disable:max-line-length
+                        // According to LSP spec,
+                        // https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_completion
+                        // if filterText is falsy, the `label` should be used.
+                        // But we observed that's not the case.
+                        // Even if vscode picked the label value, that would
+                        // cause to reorder candiates, which is not ideal.
+                        // Force to use non-empty `label`.
+                        // https://github.com/golang/vscode-go/issues/441
+                        hardcodedFilterText = items[0].label;
+                    }
+                    for (const item of items) {
+                        item.filterText = hardcodedFilterText;
+                    }
+                }
+                // TODO(hyangah): when v1.42+ api is available, we can simplify
+                // language-specific configuration lookup using the new
+                // ConfigurationScope.
+                //    const paramHintsEnabled = vscode.workspace.getConfiguration(
+                //          'editor.parameterHints',
+                //          { languageId: 'go', uri: document.uri });
+                const editorParamHintsEnabled = vscode.workspace.getConfiguration('editor.parameterHints', document.uri)['enabled'];
+                const goParamHintsEnabled = vscode.workspace.getConfiguration('[go]', document.uri)['editor.parameterHints.enabled'];
+                let paramHintsEnabled = false;
+                if (typeof goParamHintsEnabled === 'undefined') {
+                    paramHintsEnabled = editorParamHintsEnabled;
+                }
+                else {
+                    paramHintsEnabled = goParamHintsEnabled;
+                }
+                // If the user has parameterHints (signature help) enabled,
+                // trigger it for function or method completion items.
+                if (paramHintsEnabled) {
+                    for (const item of items) {
+                        if (item.kind === vscode.CompletionItemKind.Method || item.kind === vscode.CompletionItemKind.Function) {
+                            item.command = {
+                                title: 'triggerParameterHints',
+                                command: 'editor.action.triggerParameterHints'
+                            };
+                        }
+                    }
+                }
+                console.log('result list', JSON.stringify(list));
+                return list;
+            }),
+            // Keep track of the last file change in order to not prompt
+            // user if they are actively working.
+            didOpen: (e, next) => {
+                next(e);
+            },
+            didChange: (e, next) => {
+                next(e);
+            },
+            didClose: (e, next) => {
+                next(e);
+            },
+            didSave: (e, next) => {
+                console.log('save');
+                next(e);
+            },
+        }
+    };
+    exports.languageClient = new node_1.LanguageClient("funnypls", {
+        command: "funny",
+        args: ["lsp"],
+    }, clientOptions, true);
+    exports.languageClient.start();
+    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {
+    if (!exports.languageClient) {
+        return undefined;
+    }
+    return exports.languageClient.stop();
+}
+exports.deactivate = deactivate;
 //# sourceMappingURL=extension.js.map
--- src/extension.ts
+++ src/extension.ts
@@ -1,95 +1,178 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import cp = require('child_process');
-import path = require('path');
-import os = require('os');
-import fs = require('fs');
+import { workspace } from 'vscode';
+import { CloseAction, ErrorAction, LanguageClient, LanguageClientOptions, Message, ProvideCompletionItemsSignature } from 'vscode-languageclient/node';
 
-class FunnyDocumentFormatter implements vscode.DocumentFormattingEditProvider {
-	provideDocumentFormattingEdits(document: vscode.TextDocument,
-		options: vscode.FormattingOptions,
-		token: vscode.CancellationToken):
-		vscode.ProviderResult<vscode.TextEdit[]> {
-		console.log('formatting')
-		return new Promise<vscode.TextEdit[]>((resolve, reject) => {
-			let t0 = Date.now();
-			// let env = getToolsEnvVars();
-			let stdout = '';
-			let stderr = '';
+export let languageClient: LanguageClient;
+let crashCount = 0;
 
-			// Use spawn instead of exec to avoid maxBufferExceeded error
-			let funnyPath = process.env['FUNNY_BIN']
-			if (!funnyPath) {
-				let goPath = process.env['GOPATH'];
-				if (!goPath) {
-					goPath = path.join(os.homedir(), 'go')
-				} 
-				funnyPath = path.join(goPath, 'bin', 'funny')
-				if (!fs.existsSync(funnyPath)) {
-					console.error(`funny path ${funnyPath} not exists`)
-				}
-			}
-
-			console.log(funnyPath)
-			const p = cp.spawn(funnyPath, ['--format']);
-			p.stdout.setEncoding('utf8');
-			p.stdout.on('data', data => {
-				console.log(data)
-				stdout += data
-			});
-			p.stderr.on('data', data => {
-				stderr += data
-			});
-			p.on('error', err => {
-				if (err && (<any>err).code === 'ENOENT') {
-					// promptForMissingTool(formatTool);
-					console.log(err)
-					return reject();
-				}
-			});
-			p.on('close', code => {
-				if (code !== 0) {
-					console.log(stderr)
-					return reject(stderr);
-				}
-
-				// Return the complete file content in the edit.
-				// VS Code will calculate minimal edits to be applied
-				const fileStart = new vscode.Position(0, 0);
-				const fileEnd = document.lineAt(document.lineCount - 1).range.end;
-				const textEdits: vscode.TextEdit[] = [new vscode.TextEdit(new vscode.Range(fileStart, fileEnd), stdout)];
-
-				let timeTaken = Date.now() - t0;
-				/* __GDPR__
-					 "format" : {
-						"tool" : { "classification": "SystemMetaData", "purpose": "FeatureInsight" },
-						"timeTaken": { "classification": "SystemMetaData", "purpose": "PerformanceAndHealth", "isMeasurement": true }
-					 }
-				 */
-				// sendTelemetryEvent('format', { tool: formatTool }, { timeTaken });
-				if (timeTaken > 750) {
-					console.log(`Formatting took too long(${timeTaken}ms). Format On Save feature could be aborted.`);
-				}
-				return resolve(textEdits);
-			});
-			p.stdin.end(document.getText());
-		})
-	}
+// errorKind refers to the different possible kinds of gopls errors.
+enum errorKind {
+	initializationFailure,
+	crash,
+	manualRestart
 }
 
-class FunnyOnTypingFormatter implements vscode.OnTypeFormattingEditProvider {
-	provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, ch: string, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.ProviderResult<vscode.TextEdit[]> {
-		throw new Error("Method not implemented.");
-	}
-}
-
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(
-		vscode.languages.registerDocumentFormattingEditProvider(
-			'funny', new FunnyDocumentFormatter()));
-	// context.subscriptions.push(
-	// 	vscode.languages.registerOnTypeFormattingEditProvider('funny', new FunnyOnTypingFormatter(), '\n'));
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "vscode-funny" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('vscode-funny.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from vscode-funny!');
+	});
+
+	console.log('Congratulations, your extension "vscode-funny" is now active!');
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'funny' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		},
+		initializationFailedHandler: (err) => {
+			console.log(err);
+			return true;
+		},
+		errorHandler: {
+			error: (error: Error, message: Message, count: number): ErrorAction => {
+				console.log('Congratulations, your extension "vscode-funny" is now errro!');
+				// Allow 5 crashes before shutdown.
+				if (count < 5) {
+					return ErrorAction.Continue;
+				}
+				vscode.window.showErrorMessage(
+					`Error communicating with the language server: ${error}: ${message}.`
+				);
+				return ErrorAction.Shutdown;
+			},
+			closed: (): CloseAction => {
+				// Allow 5 crashes before shutdown.
+				crashCount++;
+				if (crashCount < 5) {
+					return CloseAction.Restart;
+				}
+				console.log(
+					'The connection to funnypls has been closed. The funnypls server may have crashed.',
+					errorKind.crash
+				);
+				return CloseAction.DoNotRestart;
+			}
+		},
+		middleware: {
+			provideCompletionItem: async (
+				document: vscode.TextDocument,
+				position: vscode.Position,
+				context: vscode.CompletionContext,
+				token: vscode.CancellationToken,
+				next: ProvideCompletionItemsSignature
+			) => {
+				const list = await next(document, position, context, token);
+				if (!list) {
+					return list;
+				}
+				const items = Array.isArray(list) ? list : list.items;
+
+				// Give all the candidates the same filterText to trick VSCode
+				// into not reordering our candidates. All the candidates will
+				// appear to be equally good matches, so VSCode's fuzzy
+				// matching/ranking just maintains the natural "sortText"
+				// ordering. We can only do this in tandem with
+				// "incompleteResults" since otherwise client side filtering is
+				// important.
+				if (!Array.isArray(list) && list.isIncomplete && list.items.length > 1) {
+					let hardcodedFilterText = items[0].filterText;
+					if (!hardcodedFilterText) {
+						// tslint:disable:max-line-length
+						// According to LSP spec,
+						// https://microsoft.github.io/language-server-protocol/specifications/specification-current/#textDocument_completion
+						// if filterText is falsy, the `label` should be used.
+						// But we observed that's not the case.
+						// Even if vscode picked the label value, that would
+						// cause to reorder candiates, which is not ideal.
+						// Force to use non-empty `label`.
+						// https://github.com/golang/vscode-go/issues/441
+						hardcodedFilterText = items[0].label;
+					}
+					for (const item of items) {
+						item.filterText = hardcodedFilterText;
+					}
+				}
+				// TODO(hyangah): when v1.42+ api is available, we can simplify
+				// language-specific configuration lookup using the new
+				// ConfigurationScope.
+				//    const paramHintsEnabled = vscode.workspace.getConfiguration(
+				//          'editor.parameterHints',
+				//          { languageId: 'go', uri: document.uri });
+				const editorParamHintsEnabled = vscode.workspace.getConfiguration(
+					'editor.parameterHints',
+					document.uri
+				)['enabled'];
+				const goParamHintsEnabled = vscode.workspace.getConfiguration('[go]', document.uri)[
+					'editor.parameterHints.enabled'
+				];
+				let paramHintsEnabled = false;
+				if (typeof goParamHintsEnabled === 'undefined') {
+					paramHintsEnabled = editorParamHintsEnabled;
+				} else {
+					paramHintsEnabled = goParamHintsEnabled;
+				}
+				// If the user has parameterHints (signature help) enabled,
+				// trigger it for function or method completion items.
+				if (paramHintsEnabled) {
+					for (const item of items) {
+						if (item.kind === vscode.CompletionItemKind.Method || item.kind === vscode.CompletionItemKind.Function) {
+							item.command = {
+								title: 'triggerParameterHints',
+								command: 'editor.action.triggerParameterHints'
+							};
+						}
+					}
+				}
+				console.log('result list', JSON.stringify(list));
+				return list;
+			},
+			// Keep track of the last file change in order to not prompt
+			// user if they are actively working.
+			didOpen: (e, next) => {
+				next(e);
+			},
+			didChange: (e, next) => {
+				next(e);
+			},
+			didClose: (e, next) => {
+				next(e);
+			},
+			didSave: (e, next) => {
+				console.log('save');
+				next(e);
+			},
+		}
+	};
+
+	languageClient = new LanguageClient("funnypls", {
+		command: "funny",
+		args: ["lsp"],
+	}, clientOptions, true);
+	languageClient.start();
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+	if (!languageClient) {
+		return undefined;
+	}
+	return languageClient.stop();
 }
